b8c3eeb82651eef4d13a31a82551f480ccc4d76a
Abhishek Tripathi <abhishek3.6@gmail.com>
2021-02-27 05:05:14 +0000

    added code to avoid model to calculate backpropagation as we are only interested in inference  after putting model in eval mode

--- serve/predict.py
+++ serve/predict.py
@@ -85,7 +85,8 @@ def predict_fn(input_data, model):
 
     # TODO: Compute the result of applying the model to the input data. The variable `result` should
     #       be a numpy array which contains a single integer which is either 1 or 0
-
+    ## dont need to accumulate gradients for inference
+    torch.no_grad()
     result = model(data)
     result = result.detach().numpy()
 
